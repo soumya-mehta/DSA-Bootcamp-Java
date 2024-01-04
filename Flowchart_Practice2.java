5a9c26f14b96ca6107a472b359116f060e90ce32
Soumya Mehta <80755747+soumya-mehta@users.noreply.github.com>
2024-01-04 21:35:40 +0530

    Take two numbers and print the sum of both.
    Take two numbers and print the sum of both.

---
+++ Flowchart_Practice2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Flowcharts_Practice2 {
+    //q2 Take two numbers and print the sum of both.
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your 1st number");
+        a = sc.nextInt();
+        System.out.println("Enter your 2nd number");
+        b = sc.nextInt();
+        c = a + b;
+        System.out.print("Your sum is " + c);
+        sc.close();
+    }
+}
