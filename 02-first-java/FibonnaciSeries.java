a1a2545f6716b40b8ba7e9e4afca8a9eda689ad7
Soumya Mehta <80755747+soumya-mehta@users.noreply.github.com>
2024-01-07 15:31:29 +0530

    To calculate Fibonacci Series up to n numbers.
    To calculate Fibonacci Series up to n numbers.

---
+++ 02-first-java/FibonnaciSeries.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class FibonnaciSeries {
+    //q7 To calculate Fibonacci Series up to n numbers.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int a = 0;
+        int b = 1;
+        System.out.println("Enter number n: ");
+        n = sc.nextInt();
+        sc.close();
+        System.out.println("The fibonacci series till " + n + " terms is: ");
+        System.out.print(a + " " + b + " ");
+        int i = 3;
+        while ( i <= n) {
+            int next = a + b;
+            System.out.print(next + " ");
+            a =b;
+            b = next;
+            i++;
+        }
+    }
+        
+}
