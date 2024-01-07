dfa4717ab9691a5fa4b7694b4bd2eec019556b5e
Soumya Mehta <80755747+soumya-mehta@users.noreply.github.com>
2024-01-07 14:04:34 +0530

    To find out whether the given String is Palindrome or not.
    To find out whether the given String is Palindrome or not.

--- FirstJava8.java
+++ FirstJava8.java
@@ -5,20 +5,12 @@ public class FirstJava8 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your string here: ");
         String s = sc.nextLine();
-        //if it was to check palindrome for a number- BINARY SEARCH would be more optimal
-        //but for strings, reversing and then checking is better
-        //lets store this in a buffer variable s1
         String s1= s;
-        //lets reverse it and put it in another variable s2
         String s2="";
-        //lets calculate the length to initialize the reversal of string
         int len = s.length();
-
         for (int i = len-1; i>=0; i--) {
             s2 = s2 + s.charAt(i);
         }
-        //System.out.println(s2);
-        //lets compare both the strings for palindrome
         if (s2.equals(s1)) {
             System.out.println("Its a palindrome");
         }
