c06e0479368713d841e33c0865016d0c0bcc837d
Soumya Mehta <80755747+soumya-mehta@users.noreply.github.com>
2024-01-07 15:09:30 +0530

    Write a program to print whether a number is even or odd, also take input from the user.
    Write a program to print whether a number is even or odd, also take input from the user.

---
+++ 01-flow-of-program/Leap_Year.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Leap_Year {
+    //q1 Write a program to print whether a number is even or odd, also take input from the user.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Your number is even");
+        }
+        else {
+            System.out.println("Your number is odd");
+        }
+        sc.close();
+    }
+}
