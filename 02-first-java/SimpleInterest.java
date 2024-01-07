67fb971e8b732f666fc77742d55b146ad1376211
Soumya Mehta <80755747+soumya-mehta@users.noreply.github.com>
2024-01-07 15:20:55 +0530

    Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
    Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.

---
+++ 02-first-java/SimpleInterest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SimpleInterest {
+    //q3 Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p, t, r;
+        System.out.println("Enter your input principal here: ");
+        p = sc.nextInt();
+        System.out.println("Enter the time: ");
+        t = sc.nextInt();
+        System.out.println("Enter rate of interest here: ");
+        r = sc.nextInt();
+        int i = (p*t*r)/100;
+        System.out.println("Simple interest comes as " + i);
+        sc.close();
+    }
+}
